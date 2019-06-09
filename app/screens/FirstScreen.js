5e97939142cbe61fdf1779aac854e34a8a19a96e
xavier-castro <xavier_castro@icloud.com>
2019-06-08 22:17:47 -0500

    Fix flex

--- app/screens/FirstScreen.js
+++ app/screens/FirstScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const FirstScreen = () => <View style={{ backgroundColor: 'red' }} />;
+const FirstScreen = () => <View style={{ backgroundColor: 'red', display: 'flex', flex: 1 }} />;
 
 export default FirstScreen;
