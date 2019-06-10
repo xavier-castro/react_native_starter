434d7672142f7ac3f7fda99d558b26a2105be96b
xavier-castro <xavier_castro@icloud.com>
2019-06-10 16:30:56 -0500

    Change icons bast on platform

---
+++ app/screens/Options.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { ScrollView, StatusBar, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import { ListItem, Separator } from '../components/Lists';
+
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
+
+class Options extends Component {
+  handleThemesPressed = () => {
+    console.log('Handle themes pressed');
+  };
+
+  handleSitePressed = () => {
+    console.log('Handle site pressed');
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar translucent={false} barStyle="default" />
+        <ListItem
+          text="Themes"
+          onPress={this.handleThemesPressed}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-arrow-forward`} color={ICON_COLOR} size={ICON_SIZE} />
+          }
+        />
+        <Separator />
+        <ListItem
+          text="Fixer.io"
+          onPress={this.handleSitePressed}
+          customIcon={<Ionicons name={`${ICON_PREFIX}-link`} color={ICON_COLOR} size={ICON_SIZE} />}
+        />
+        <Separator />
+      </ScrollView>
+    );
+  }
+}
+
+export default Options;
